52262062f34fac5e050b7e5e0cb2862219c0fccb
PriyanshSinghBisht <priyanshbisht5@gmail.com>
2025-08-25 10:45:05 +0000

    removed mandatory from some distributor and employee fields

---
+++ lms/lms/doctype/employee/employee.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Frappe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Employee(NestedSet):
+	pass
